93c434ea35248416bd610b28cb71d8cd53dfc187
edsonjuniornarvaes <edsonjunior.narvaes@gmail.com>
2020-07-09 23:31:22 -0300

    connection pdo

--- app/models/Connection.php
+++ app/models/Connection.php
@@ -1,22 +1,17 @@
 <?php
 
-namespace app\models;
+namespace app\models ;
+use PDO;
 
 
 class Connection {
-    
-    $dsn = "mysql:host=localhost;dbname=database";
-    $dsn = "mysql:host=mysql;port=3307;dbname=database";
-    $dbuser = "root";
-    $dbpass = "12345";
 
     public static function connect() {
 
-        $pdo = new PDO($dsn,$dbuser, $dbpass);
-        echo "Conectado!";
+        $pdo = new PDO("mysql:host=mysql;dbname=database","root", "12345");
 
-        // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        // $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
         return $pdo;
     }
