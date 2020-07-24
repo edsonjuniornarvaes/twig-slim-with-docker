269e64605acee623c2eb361a36df12e0ccff509e
edsonjuniornarvaes <edsonjunior.narvaes@gmail.com>
2020-07-23 22:05:57 -0300

    Creating pagination class and readme 0.1

---
+++ app/models/Paginate.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\models;
+
+class Paginate 
+{
+    private $page;
+    private $perPage;
+    private $offset;
+    private $pages;
+    private $records;
+
+    private function current() 
+    {
+        $this->page = $_GET['page'] ?? 1;
+    }
+
+    private function perPage($perPage) 
+    {
+        $this->perPage = $perPage ?? 30;
+    }
+
+    private function offset()
+    {
+        $this->offset = ($this->page * $this->perPage) - $this->perPage;
+    }
+
+    public function records($records)
+    {
+        $this->records = $records;
+    }
+
+    private function pages()
+    {
+        $this->pages = ceil($this->records / $this->perPage);
+    }
+
+    public function sqlPaginate()
+    {
+        return " limit {$this->perPage} offset {$this->offset}";
+    }
+
+    public function paginate($perPage) 
+    {
+        $this->current();
+        $this->perPage($perPage);
+        $this->offset();
+        $this->pages();
+
+        return $this;
+
+    }
+}
